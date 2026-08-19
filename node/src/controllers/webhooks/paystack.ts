dc22031ac133ee418df7b71bcf0f8b3fa5e1ccb6
AKPULU FABIAN CHINEDU <fabianakpulu@icloud.com>
2026-08-19 22:11:22 +0100

    testing webhook from new render domain

--- node/src/controllers/webhooks/paystack.ts
+++ node/src/controllers/webhooks/paystack.ts
@@ -20,7 +20,6 @@ export async function PaystackWebhookController(req: Request, res: Response): Pr
 
   try {
 
-    console.log("testing webhook from new render account...")
     if(!secret){
       res.status(500).send("Missing Paystack secret");
       return;
@@ -98,26 +97,26 @@ export async function PaystackWebhookController(req: Request, res: Response): Pr
     const { metadata, reference } = paystackData;
   
     // Process only successful charges
-    if (eventType !== "charge.success") {
-      if(eventType === "transfer.success"){
-        escrow.complete({
-          status: "success", 
-          transfer_reference: reference, 
-        });
-      }else if(eventType === "transfer.failed"){
-        escrow.complete({
-          status: "failed", 
-          transfer_reference: reference, 
-        });
-      }else if(eventType === "transfer.reversed"){
-        escrow.complete({
-          status: "reversed", 
-          transfer_reference: reference, 
-        });
-      }else{
-        res.status(200).send("Ignored");
-      }
-    }
+    // if (eventType !== "charge.success") {
+    //   if(eventType === "transfer.success"){
+    //     escrow.complete({
+    //       status: "success", 
+    //       transfer_reference: reference, 
+    //     });
+    //   }else if(eventType === "transfer.failed"){
+    //     escrow.complete({
+    //       status: "failed", 
+    //       transfer_reference: reference, 
+    //     });
+    //   }else if(eventType === "transfer.reversed"){
+    //     escrow.complete({
+    //       status: "reversed", 
+    //       transfer_reference: reference, 
+    //     });
+    //   }else{
+    //     res.status(200).send("Ignored");
+    //   }
+    // }
 
     // const { metadata, reference } = paystackData;
     const { customer_id, shipping_address, tax, orders } = metadata || {};
